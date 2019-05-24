812cab1c5fbad477547bb21a7ff4a88617da5165
wenyilu <123456>
2019-05-24 17:19:56 +0800

    修改配置文件

--- ibsaas-rocketmq/src/main/java/com/landleaf/ibsaas/rocketmq/TopicConstants.java
+++ ibsaas-rocketmq/src/main/java/com/landleaf/ibsaas/rocketmq/TopicConstants.java
@@ -4,6 +4,7 @@ public class TopicConstants {
     /**
      * 门禁数据返回消费topic
      */
-    public final static String TOPIC_KNIGHT = "knight_test";
+    public final static String TOPIC_KNIGHT = "knight";
+    public final static String TOPIC_KNIGHT_TEST = "knight_test";
 
 }
