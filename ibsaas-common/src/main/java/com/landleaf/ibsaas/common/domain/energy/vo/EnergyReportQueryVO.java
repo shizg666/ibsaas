c10edafe10ec22b8f6b198b3c296aaa2e0e5ba20
shizg <shizengguang@landleaf-tech.com>
2019-06-20 16:20:27 +0800

    修改优化

---
+++ ibsaas-common/src/main/java/com/landleaf/ibsaas/common/domain/energy/vo/EnergyReportQueryVO.java
@@ -0,0 +1,29 @@
+package com.landleaf.ibsaas.common.domain.energy.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@ApiModel("能源报表查询对象")
+public class EnergyReportQueryVO implements Serializable {
+
+
+    @ApiModelProperty("时间维度 1-时 2-日 3-月 4-年")
+    private Integer dateType;
+    @ApiModelProperty("开始时间")
+    private String startTime;
+    @ApiModelProperty("结束时间")
+    private String endTime;
+    @ApiModelProperty("能耗类型 1-水 2-电")
+    private Integer energyType;
+    @ApiModelProperty("查询维度 1-分区 2-分项 ")
+    private Integer queryType;
+    @ApiModelProperty("查询值")
+    private Integer queryValue;
+
+
+}
