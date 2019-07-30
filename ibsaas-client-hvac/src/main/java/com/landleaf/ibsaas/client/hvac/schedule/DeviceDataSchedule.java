adb6acfb50def18058aec4739cbf60770f48b00c
lujingyao <ljy88808@gmail.com>
2019-07-30 16:44:53 +0800

    人员:lujingyao 分支:data0.1 描述:配置

--- ibsaas-client-hvac/src/main/java/com/landleaf/ibsaas/client/hvac/schedule/DeviceDataSchedule.java
+++ ibsaas-client-hvac/src/main/java/com/landleaf/ibsaas/client/hvac/schedule/DeviceDataSchedule.java
@@ -27,11 +27,9 @@ public class DeviceDataSchedule {
     public void dataToRedis(){
 //        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>开始刷入redis数据<<<<<<<<<<<<<<<<<<<<<<<<<");
 //        log.info(">>>>>>>>>>>>>>>>>>>>>>>>>开始刷入redis数据<<<<<<<<<<<<<<<<<<<<<<<<<");
-        long start = System.currentTimeMillis();
         iCommonDeviceService.currentDataToRedis();
-        long t = System.currentTimeMillis() - start;
 //        System.out.println("------------------------------>时间为"+t+"毫秒");
-        log.info("刷device数据------------------------------>时间为{}毫秒", t);
+        log.info("-------------------------多线程刷device数据-------------------------");
 //        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>刷入redis数据结束<<<<<<<<<<<<<<<<<<<<<<<<<");
 //        log.info(">>>>>>>>>>>>>>>>>>>>>>>>>刷入redis数据结束<<<<<<<<<<<<<<<<<<<<<<<<<");
     }
