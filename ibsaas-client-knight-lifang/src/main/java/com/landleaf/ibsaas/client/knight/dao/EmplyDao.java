5d39806084615d1b0e7cf4305b73500cfe6d0636
温奕禄 <wenyilu@landleaf-tech.com>
2020-04-02 13:09:44 +0800

    移除冗余错误项目

--- ibsaas-client-knight-lifang/src/main/java/com/landleaf/ibsaas/client/knight/dao/EmplyDao.java
+++
@@ -1,9 +0,0 @@
-package com.landleaf.ibsaas.client.knight.dao;
-
-import com.landleaf.ibsaas.client.knight.domain.Emply;
-import com.landleaf.ibsaas.datasource.mybatis.basedao.BaseDao;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmplyDao extends BaseDao<Emply> {
-}
--- ibsaas-client-knight-lifang/src/main/java/com/landleaf/ibsaas/client/knight/dao/KQCardRecordDao.java
+++
@@ -1,9 +0,0 @@
-package com.landleaf.ibsaas.client.knight.dao;
-
-import com.landleaf.ibsaas.client.knight.domain.KQCardRecord;
-import com.landleaf.ibsaas.datasource.mybatis.basedao.BaseDao;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KQCardRecordDao extends BaseDao<KQCardRecord> {
-}
